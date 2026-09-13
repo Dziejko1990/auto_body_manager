17d34e4b2733f31ca2151b9ab438ecc263dac183
Dziejko1990 <andrzej90_17@o2.pl>
2026-09-13 14:17:24 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='abm-pro12-v1';
-const CORE=['/','/index.html','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+const CACHE='abm-pro12-6-brand-v1';
+const CORE=['/','/index.html','/manifest.webmanifest?v=126','/apple-touch-icon.png?v=126','/icon-192.png?v=126','/icon-512.png?v=126'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
